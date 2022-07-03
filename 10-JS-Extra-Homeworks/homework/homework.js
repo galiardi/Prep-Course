dcf5d03bb293a7e20c85a022e15daee426902b71
galiardi <galiardi.dev@gmail.com>
2022-07-02 21:09:05 -0400

    modifies JS extra homework, sortArray function

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -102,7 +102,7 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-  const newArray = [];
+  /*const newArray = [];
   while (arr.length > 0) {
     let menor = Infinity;
     let index;
@@ -118,6 +118,8 @@ function sortArray(arr) {
     arr.splice(index, 1);
   }
   return newArray;
+  */
+  return arr.sort((a, b) => a.length - b.length);
 }
 
 function buscoInterseccion(arreglo1, arreglo2) {
